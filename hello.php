4feee7dceb0f7094d7ce79fee13e29517bc56caf
tgl123 <329358447@qq.com>
2016-03-28 20:48:21 +0800

    模定义<<<

--- hello.php
+++ hello.php
@@ -1,9 +1,10 @@
 <?php
 echo "hello world";
-<<<<<<< HEAD
+<<< HEAD
 echo "<br />";//测试：次文件加的linux服务器上添加的
 =======
 //tesing
 >>>>>>> 47b99f17c13bba9a5524cbe4341ee3d0f7c6cbb8
 //Can you help me ?
+HEAD
 ?>
